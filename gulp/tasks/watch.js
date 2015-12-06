abd7b89f23ae8f3c1d5695ed9bd93657a8ea2070
Rick van Mook <rick@rvmook.com>
2015-12-06 01:02:06 -0500

    Added html watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,5 +8,6 @@ module.exports = function(taskName) {
 		// There's no watch task for Browserify. It's handling updates with Watchify
 		gulp.watch(config.styles.src, ['styles']);
 		gulp.watch(config.copyStaticAssets.src, ['copyStaticAssets']);
+		gulp.watch(config.html.src, ['html']);
 	});
 };
